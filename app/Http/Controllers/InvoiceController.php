437cef950645348783dc2098e6317bf8136c9112
ichsan arrizqi <ichsanarrizqi090@gmail.com>
2021-09-15 00:53:41 -0700

    the invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\CaseList;
+use App\Models\Client;
 use App\Models\Invoice;
 use App\Models\MemberInsurance;
+use App\Models\NoInvoice;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InvoiceController extends Controller
 {
@@ -25,7 +30,24 @@ class InvoiceController extends Controller
 
     public function store(Request $request)
     {
-        //
+        try {
+            DB::beginTransaction();
+            foreach (CaseList::find($request->no_case)->member as $data) {
+                NoInvoice::create([
+                    'no_invoice' => $data->caselist->file_no,
+                    'due_date' => $data->caselist->end,
+                    'date_invoice' => Carbon::now()->format('Y-m-d'),
+                    'status_paid' => 1,
+                    'created_by' => auth()->user()->id,
+                    'is_active' => 1
+                ]);
+            }
+            DB::commit();
+        } catch (Exception $err) {
+            DB::rollBack();
+            dd($err->getMessage());
+        }
+        return back()->with('success', 'Berhasil');
     }
 
     public function show(Invoice $invoice)
